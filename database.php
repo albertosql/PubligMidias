56b6c1353450537e3d3886a59e90116d3e51d951
albertosql <albertosql@hotmail.com>
2016-12-22 01:13:58 -0400

    Integrando o site com o módulo administrativo.

--- database.php
+++ database.php
@@ -40,19 +40,75 @@ function findNumeros() {
 function findMidias() {
 
     $database = open_database();
-    $found = null;
+
     try {
         $sql = "SELECT `nome` FROM `tipos_midias`";
         $result = $database->query($sql);
 
-        if ($result->num_rows > 0) {
-            $found = $result->fetch_assoc();
+        while ($row = $result->fetch_assoc()) {
+            $tipoMidia = utf8_encode($row['nome']);
+            $tipoMidia = str_replace(" ","",$tipoMidia);
+            echo "<button class='filter' data-filter='.".$tipoMidia."'>".utf8_encode($row['nome'])."</button>";
         }
+
     } catch (Exception $e) {
         $_SESSION['message'] = $e->GetMessage();
         $_SESSION['type'] = 'danger';
     }
-
     close_database($database);
-    return $found;
 }
+
+function findPortfolio() {
+
+    $database = open_database();
+
+    try {
+        $sql = "SELECT * FROM `portfolio`";
+        $result = $database->query($sql);
+
+        foreach ($result as $servico):
+            $id = $servico['id'];
+            $empresa = utf8_encode($servico['empresa']);
+            $texto = utf8_encode($servico['texto']);
+            $midia = utf8_encode($servico['tipo']);
+            $foto = $servico['foto'];
+
+            $nomeimagem = str_replace(" ","",$empresa);
+
+            $tipoMidia = str_replace(" ","",$midia);
+            $tipoMidia = preg_replace( '/[`^~\'"]/', null, iconv( 'UTF-8', 'ASCII//TRANSLIT', $tipoMidia ) );
+
+            echo "<div class='col-lg-4 col-md-6 col-sm-4 col-xs-6 mix ".$tipoMidia."' style='display: inline-block';>";
+                echo "<div class='project'>";
+                    echo "<img src='assets/".$foto."' alt='".$empresa."'>";
+                    echo "<div class='ovrly'>";
+                    echo "</div>";
+                    echo "<div class='buttons'>";
+                    echo "<a href='#".$id."' class='fa fa-eye show-popup'></a>";
+                    echo "</div>";
+                echo "</div>";
+            echo "</div>";
+
+            echo "<div class='pop-up-box' id='".$id."'>";
+                echo "<img src='assets/".$foto."' alt='".$empresa."' class='hidden-xs'>";
+                echo "<div class='popup-content'>";
+                    echo "<h3>".$empresa."</h3>";
+                    echo "<p>";
+                        echo $texto;
+                    echo "</p>";
+                echo "</div>";
+            echo "</div>";
+
+        endforeach;
+
+    } catch (Exception $e) {
+        $_SESSION['message'] = $e->GetMessage();
+        $_SESSION['type'] = 'danger';
+    }
+    close_database($database);
+}
+
+?>
+
+
+
